60fb199d7a91ba45039f9326cf817f7ffef4ba46
vttoteva25 <stu2201322010@uni-plovdiv.bg>
2023-11-18 00:03:52 +0200

    add HelpingTools

---
+++ LibraryArchive/HelpingTools/Hasher.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using XSystem.Security.Cryptography;
+
+namespace LibraryArchive.HelpingTools
+{
+    public static class Hasher
+    {
+        public static string Hash(string password)
+        {
+            var data = Encoding.ASCII.GetBytes(password);
+            var md5 = new MD5CryptoServiceProvider();
+            var md5data = md5.ComputeHash(data);
+            string hashedPassword = new ASCIIEncoding().GetString(md5data);
+            md5.Clear();
+            return hashedPassword;
+        }
+    }
+}
---
+++ LibraryArchive/HelpingTools/Logged.cs
@@ -0,0 +1,9 @@
+﻿using LibraryArchive.Models;
+
+namespace LibraryArchive.HelpingTools
+{
+    public static class Logged
+    {
+        public static Librarian Librarian { get; set; }
+    }
+}
